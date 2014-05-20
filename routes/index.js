5fab579e5362bf2477a63a97bd0cd2498ffbf19b
madhavmurthy93 <madhavmurthy93@yahoo.com>
2014-05-20 16:08:48 -0700

    Displaying views and answers correctly.

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,77 @@ exports.index = function(req, res) {
 
 // get questions based on sort requested
 exports.search = function(req, res) {
+	var json = [];
+	var urls = get_urls(req.params.tag, req.query.limit, req.path);
+	async.eachSeries(urls, function(url, callback) {
+		request(url, function(err, response, body) {
+			if(err) console.log(err);
+			var $ = cheerio.load(body);
+			$('#questions .question-summary').each(function() {
+				var question_summary = $(this);
+				var stats = question_summary.children('.statscontainer');
+				var answers = stats.children('.stats').children('.status ').text();
+				answers = answers.match(/[0-9]+/)[0];
+				var views = stats.children('.views ').text().trim().split(" ");
+				views = views[0];
+				var id = question_summary.attr('id').split('-')[2];
+				var summary = question_summary.children('.summary');
+				var question = summary.children('h3').children('a').text();
+				var link = 'http://www.stackoverflow.com/questions/' + id;
+				var tags = '';
+				summary.children('.tags ').children('a').each(function() {
+					tags = tags + $(this).text() + ' ';
+				});
+				tags = tags.trim();
+				var user_details = summary.children('.started').children('.user-info ').children('.user-details');
+				if(user_details.first().children('.community-wiki').html() != null) {
+					json.push({'question-id': id, 
+						'views': views,
+						'answers': answers,
+						'question': question, 
+						'link': link,
+						'tags': tags,
+						'user': 'community-wiki - community owned post',
+						'reputation-score': 'no rep score'});
+				} else {
+					var user = user_details.children('a').text();
+					var rep = user_details.children('.reputation-score').text();
+					json.push({'question-id': id, 
+						'views': views,
+						'answers': answers,
+						'question': question, 
+						'link': link,
+						'tags': tags,
+						'user': user,
+						'reputation-score': rep});
+				}
+			});
+		callback();
+		});
+	}, function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.set({'content-type': 'application/json; charset=utf-8'});
+			res.json({'questions': json});
+		}
+	});
+};
+
+// gets array of urls based on user tag and questions limit input
+function get_urls(tag_given, limit, path) {
 	var urls = [];
 	var base_url = 'http://www.stackoverflow.com/questions';
-	var path = req.path;
 	var sort = path.split('/')[2];
 	var questions = 10;
 	var pages = 0;
-	if(req.query.limit) {
-		questions = req.query.limit;
+	if(limit) {
+		questions = limit;
 		pages = Math.floor(questions / 50);
 	}
 	var tag = '';
-	if(req.params.tag) {
-		tag = req.params.tag;
+	if(tag_given) {
+		tag = tag_given;
 		var i;
 		for(i = 1; i <= pages; i++) {
 			url = base_url + '/tagged/' + tag + '?page=' + i + '&sort=' + sort + '&pageSize=50';
@@ -43,61 +101,8 @@ exports.search = function(req, res) {
 			urls.push(base_url + '?page=' + i + '&sort=' + sort + '&pageSize=' + r);
 		}
 	}
-	var json = [];
-	async.eachSeries(urls, function(url, callback) {
-		request(url, function(err, response, body) {
-			if(err) console.log(err);
-			var $ = cheerio.load(body);
-			$('#questions .question-summary').each(function() {
-				var question_summary = $(this);
-				var stats = question_summary.children('.statscontainer');
-				var answers = stats.children('.stats').children('.status ').text();
-				answers = answers.match(/[0-9]+/)[0];
-				var views = stats.children('.views ').text();
-				views = views.match(/[0-9]+/)[0];
-				var id = question_summary.attr('id').split('-')[2];
-				var summary = question_summary.children('.summary');
-				var question = summary.children('h3').children('a').text();
-				var link = 'http://www.stackoverflow.com/questions/' + id;
-				var tags = '';
-				summary.children('.tags ').children('a').each(function() {
-					tags = tags + $(this).text() + ' ';
-				});
-				tags = tags.trim();
-				var user_details = summary.children('.started').children('.user-info ').children('.user-details');
-				if(user_details.first().children('.community-wiki').html() != null) {
-					json.push({'question-id': id, 
-						   'views': views,
-						   'answers': answers,
-						   'question': question, 
-						   'link': link,
-						   'tags': tags,
-						   'user': 'community-wiki - community owned post',
-						   'reputation-score': 'no rep score'});
-				} else {
-					var user = user_details.children('a').text();
-					var rep = user_details.children('.reputation-score').text();
-					json.push({'question-id': id, 
-							   'views': views,
-							   'answers': answers,
-							   'question': question, 
-							   'link': link,
-							   'tags': tags,
-							   'user': user,
-							   'reputation-score': rep});
-				}
-			});
-		callback();
-		});
-	}, function(err) {
-		if(err) {
-			console.log(err);
-		} else {
-			res.set({'content-type': 'application/json; charset=utf-8'});
-			res.json({'questions': json});
-		}
-	});
-};
+	return urls;
+}
 
 
 // attempt to get answers to questions. need to change.
